d3d21903d20e5c0b9e1a232fad8aa00642af80fd
Morgan Fudge <mfudge92@gmail.com>
2018-01-28 13:36:46 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180128133438) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "name"
+    t.string "content"
+  end
 
 end
