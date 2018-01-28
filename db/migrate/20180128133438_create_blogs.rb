871d36552c8bc7269da131a74d8700c6e905ccb2
Morgan Fudge <mfudge92@gmail.com>
2018-01-28 13:46:28 +0000

    automatically backed up by learn

--- db/migrate/20180128133438_create_blogs.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlogs < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :name
-      t.string :content
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128133438) do
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "name"
-    t.string "content"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
